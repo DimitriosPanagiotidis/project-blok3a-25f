e5e752ac821e6139ec9a922143899052142d76d7
David van Oudheusden <doudheusden@novacollege.nl>
2025-09-16 17:03:35 +0200

    new project

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Op deze pagina begint de <strong>applicatie</strong>
+</body>
+</html>
