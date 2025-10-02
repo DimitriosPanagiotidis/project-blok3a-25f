a63f2b01b6e909fb58297c344027b49288ed71e8
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2025-10-02 13:40:12 +0200

    footer and navbar ready

---
+++ www/footer.php
@@ -0,0 +1,2 @@
+<p>All rights reserved! Created by Dimitris Panagiotidis</p>
+<p>Student number : 203156</p>
---
+++ www/navbar.php
@@ -0,0 +1,25 @@
+<?php 
+$navigatie = [
+    'Home'    => 'index.php',
+    'About'   => 'about.php',
+    'Contact' => 'contact.php',
+];
+
+
+
+
+
+
+?>
+
+
+
+
+<nav class="navbar">
+    <ul>
+        <?php foreach($navigatie as $label => $link): ?>
+            <li><a href="<?php echo $link ?>"><?php echo $label ?></a></li>
+        <?php endforeach; ?>
+
+    </ul>
+</nav>
