f72310d11e836a60a7a4c1dbd8049a9040995483
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2025-11-19 11:27:38 +0100

    add destination form with multiple fields for detailed input

---
+++ www/destination.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="reset.css">
+</head>
+<body class="body">
+    <header>
+        <?php include 'navbar.php'; ?>
+    </header>
+
+    <main>        
+         <div class="form-wrapper">
+            <div>
+                <form action="create_destination.php" method="post">
+                    <label for="destination">Destination: </label>
+                    <input type="text" id="destination" name="destination" placeholder="Paris" required>
+                    
+                    <label for="country">Country:</label>
+                    <input type="text" id="country" name="country" placeholder="country" required>
+                    
+                    <label for="continent">Continent</label>
+                    <input type="text" id="continent" name="continent" placeholder="Europe" required>
+                    
+                    <label for="best_season">Best Season:</label>
+                    <input type="text" id="best_season" name="best_season" placeholder="Spring" required>
+                    
+                    <label for="average_cost_per_day">Average cost per day:</label>
+                    <input type="text" id="average_cost_per_day" name="average_cost_per_day" placeholder="100.00" required> 
+                    
+                    <label for="main_attraction">Main Attraction</label>
+                    <input type="text" id="main_attraction" name="main_attraction" placeholder="Eiffel Tower" required>
+
+                    <label for="language">Language</label>
+                    <input type="text" id="language" name="language" placeholder="English" required>
+
+                    <label for="currency">Currency</label>
+                    <input type="text" id="currency" name="currency" placeholder="Euro" required>
+                    
+                    <label for="beach_destination">Beach Destination</label>
+                    <input type="text" id="beach_destination" name="beach_destination" placeholder="1" required>
+
+                    <button type="submit">Submit</button>
+                </div>
+            </div>
+            </form>        
+        </main>
+
+    <footer class="footer">
+        <p>Created by Dimitris Panagiotidis</p>
+        <p>Student number : 203156</p>
+    </footer>
+</body>
+</html>
--- www/index.php
+++ www/index.php
@@ -1,23 +1,32 @@
 <?php
-// include 'destination-tables.php';
 require 'database.php';
-
 $sql = "SELECT * FROM destinations";
 
-$result = mysqli_query($conn,$sql);
+// FILTEREN OP CONTINENT
+if (!empty($_GET['continent'])) {
+    $continent = $_GET['continent'];
+    $sql = "SELECT * FROM destinations WHERE continent = '$continent'";
+}
+// FILTEREN OP PRIJS OPLOPEND OF AFLOPEND
+if (!empty($_GET['average_cost_per_day'])) {
+    $average_cost_per_day = $_GET['average_cost_per_day'];
+    $sql = "SELECT * FROM destinations ORDER BY average_cost_per_day ASC";
+    if ($average_cost_per_day === 'high') {
+        $sql = "SELECT * FROM destinations ORDER BY average_cost_per_day DESC";
+    }
+}
 
+
+$result = mysqli_query($conn, $sql);
 $destinations = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-
 
 
 ?>
 
 
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,51 +34,77 @@ $destinations = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="reset.css">
 </head>
+
 <body>
     <header>
-        <!-- NAVIGATION BAR -->
+        <!-- NAVBAR BAR -->
         <?php include 'navbar.php'; ?>
-        <!-- END OF NAVIGATION -->
-
-        <h1>Holiday's Paradise</h1>
-
-        <!-- SEARCH BOX -->
-         <div class="search-box">
-             <form action="search.php" method="GET">
-                 <input type="text" name="query" placeholder="Search...">
-                 <button type="button">Search</button>
-                </form>
+        <div>
+            <h1>Holiday's Paradise</h1>
         </div>
-        <!-- END OF SEARCH BOX -->
-        <h2>Destinations</h2>
+        <!-- Filter Destinations -->
+        <section class="filter-destinations">
+            <ul>
+                <li>
+                    <a href="index.php">All Destinations</a>
+                </li>
+                <li>
+                    <a href="?continent=Europe">Europe</a>
+                </li>
+                <li>
+                    <a href="?continent=Asia">Asia</a>
+                </li>
+                <li>
+                    <a href="?continent=North America">North America</a>
+                </li>
+                <li>
+                    <a href="?continent=South America">South America</a>
+                </li>
+                <li>
+                    <a href="?continent=Africa">Africa</a>
+                </li>
+                <li>
+                    <a href="?continent=Australia">Australia</a>
+                </li>
+            </ul>
+        </section>
     </header>
+    <!-- SEARCH BOX -->
+    <section class="search-box">
+        <form action="search.php" method="GET">
+            <input type="text" name="query" placeholder="Search...">
+            <button type="button">Search</button>
+        </form>
+    </section>
+    <!-- DROPDOWN MENU  -->
+    <section class="dropdown-menu">
+        <div class="dropdown">
+            <button>Prijs</button>
+            <div class="content">
+                <a href="?average_cost_per_day=low">Laag - Hoog</a>
+                <a href="?average_cost_per_day=high">Hoog - Laag</a>
+            </div>
+        </div>
+    </section>
     <!-- DESTINATIONS -->
     <main class="main-content">
         <?php foreach ($destinations as $destination): ?>
-        <section class="destination-section">
-            <div class="destination-card">
-                <h3><?php echo $destination['destination']; ?></h3>
-                <p>Country: <?php echo $destination['country']; ?></p>
-                <p>Continent: <?php echo $destination['continent']; ?></p>
-                <p>Average Cost per Day: $<?php echo number_format($destination['average_cost_per_day'], 2); ?></p>
-                <a href="detail-page.php?destinations-id=<?php echo $destination['id']; ?>">
-                    <img src="<?php echo $destination['image']; ?> "alt="<?php echo $destination['destination']; ?>">
-                </a>
-            </div>
+            <section class="destination-section">
+                <div class="destination-card">
+                    <h3><?php echo $destination['destination']; ?></h3>
+                    <p>Country: <?php echo $destination['country']; ?></p>
+                    <p>Continent: <?php echo $destination['continent']; ?></p>
+                    <p>Average Cost per Day: $<?php echo number_format($destination['average_cost_per_day'], 2); ?></p>
+                    <a href="detail-page.php?destinations-id=<?php echo $destination['id']; ?>">
+                        <img src="<?php echo $destination['image']; ?> " alt="<?php echo $destination['destination']; ?>">
+                    </a>
+                </div>
             </section>
         <?php endforeach; ?>
-     <!-- END OF DESTINATIONS  -->
     </main>
-    
-
-
-
-
-
     <footer>
         <?php include 'footer.php'; ?>
     </footer>
-
-
 </body>
+
 </html>
